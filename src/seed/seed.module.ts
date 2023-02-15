47c5f893fa83c38bbf3e005da3b0383e966789b6
ihor <ihor.bielchenko@gmail.com>
2023-02-15 18:06:49 +0100

    test

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -23,6 +23,11 @@ import { FormStatus } from '../api/form-status/form-status.entity';
 import { FieldStatus } from '../api/field-status/field-status.entity';
 import { ContentStatus } from '../api/content-status/content-status.entity';
 import { Form } from '../api/form/form.entity';
+import { FormField } from '../api/form-field/form-field.entity';
+import { FormFormFormOption } from '../api/form-form-form-option/form-form-form-option.entity';
+import { FormFormOption } from '../api/form-form-option/form-form-option.entity';
+import { FieldContent } from '../api/field-content/field-content.entity';
+import { Field } from '../api/field/field.entity';
 import { SettingSeeder } from './setting.seeder';
 import { FormStatusSeeder } from './form-status.seeder';
 import { FieldStatusSeeder } from './field-status.seeder';
@@ -39,7 +44,12 @@ import { FormSeeder } from './form.seeder';
 			FormStatus,
 			FieldStatus,
 			ContentStatus,
+			Field,
+			FormFormOption,
 			Form,
+			FormField,
+			FormFormFormOption,
+			FieldContent,
 		]),
 		ReplicaModule,
 		TransportModule,
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -6,7 +6,10 @@ import {
 } from '@nestjs/common';
 import { CacheService } from '@nest-datum/cache';
 import { SettingSeeder } from './setting.seeder';
-import { ReportStatusSeeder } from './report-status.seeder';
+import { FormStatusSeeder } from './form-status.seeder';
+import { FieldStatusSeeder } from './field-status.seeder';
+import { ContentStatusSeeder } from './content-status.seeder';
+import { FormSeeder } from './form.seeder';
 
 @Injectable()
 export class SeedService {
@@ -17,11 +20,17 @@ export class SeedService {
 		private readonly cacheService: CacheService,
 		private readonly connection: Connection,
 		private readonly settings: SettingSeeder,
-		private readonly reportStatus: ReportStatusSeeder,
+		private readonly formStatus: FormStatusSeeder,
+		private readonly fieldStatus: FieldStatusSeeder,
+		private readonly contentStatus: ContentStatusSeeder,
+		private readonly form: FormSeeder,
 	) {
 		this.seeders = [
 			this.settings,
-			this.reportStatus,
+			this.formStatus,
+			this.fieldStatus,
+			this.contentStatus,
+			this.form,
 		];
 	}
 
@@ -29,8 +38,14 @@ export class SeedService {
 		try {
 			await this.cacheService.clear([ 'setting', 'many' ]);
 			await this.cacheService.clear([ 'setting', 'one' ]);
-			await this.cacheService.clear([ 'reportStatus', 'many' ]);
-			await this.cacheService.clear([ 'reportStatus', 'one' ]);
+			await this.cacheService.clear([ 'formStatus', 'many' ]);
+			await this.cacheService.clear([ 'formStatus', 'one' ]);
+			await this.cacheService.clear([ 'fieldStatus', 'many' ]);
+			await this.cacheService.clear([ 'fieldStatus', 'one' ]);
+			await this.cacheService.clear([ 'contentStatus', 'many' ]);
+			await this.cacheService.clear([ 'contentStatus', 'one' ]);
+			await this.cacheService.clear([ 'form', 'many' ]);
+			await this.cacheService.clear([ 'form', 'one' ]);
 
 			await Bluebird.each(this.seeders, async (seeder) => {
 				this.logger.log(`Seeding ${seeder.constructor.name}`);
