a6d85d143588e48368ba3f4dedeb511c9b8a92a3
ihor <ihor.bielchenko@gmail.com>
2023-05-07 02:07:31 +0200

    Test

--- src/api/field-content/field-content-tcp.controller.ts
+++ src/api/field-content/field-content-tcp.controller.ts
@@ -8,8 +8,8 @@ import { FieldContentService } from './field-content.service';
 
 @Controller()
 export class FieldContentTcpController extends BindTcpController {
-	protected readonly mainRelationColumnName: string = 'fieldId';
-	protected readonly optionRelationColumnName: string = 'contentId';
+	protected readonly mainRelationColumnName: string = 'contentId';
+	protected readonly optionRelationColumnName: string = 'fieldId';
 	
 	constructor(
 		protected service: FieldContentService,
